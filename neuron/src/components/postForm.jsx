ce662ad8025af7c222ffce5200260999e6c4a432
mwells21 <mwells21@uic.edu>
2019-01-13 00:17:02 -0600

    submit post form

--- neuron/src/components/postForm.jsx
+++ neuron/src/components/postForm.jsx
@@ -11,16 +11,37 @@ class PostForm extends Component {
         };
 
         this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
     }
     
     onChange(e){
         this.setState({[e.target.name ]:e.target.value});
     }
 
+    onSubmit(e){
+        e.preventDefault();
+
+        const post ={
+            body: this.state.body
+        }
+
+
+        fetch('https://jsonplaceholder.typicode.com/posts',{
+            method:'POST',
+            headers:{
+                'content-type':'application/json'
+            },
+            body:JSON.stringify(post)
+        })
+        .then(res => res.json())
+        .then(data=>console.log(data))
+        this.setState({body: ''})
+    }
+
     render() { 
         return ( 
             <React.Fragment>
-                <Form>
+                <Form onSubmit={this.onSubmit}>
                     <FormGroup>
                         <Label for="makePost">Post</Label>
                         <Input type="textarea" name="body" id="makePost" 
